21e46634c9f305da3e53c4cc6a06def910efe75a
Andre Praeiro <andre.campos@sigma.com.br>
2019-10-01 17:42:51 -0400

    cart layout

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,8 +1,55 @@
 import React from 'react';
-import {View} from 'react-native';
 
-// import { Container } from './styles';
+import {
+  Container,
+  ProductLine,
+  Image,
+  NamePrice,
+  Name,
+  Price,
+  RemoveButton,
+  AmountLine,
+  DecButton,
+  Amount,
+  AddButton,
+  Subtotal,
+  TotalLine,
+  TotalTitle,
+  Total,
+  Card,
+  AmountControl,
+} from './styles';
 
 export default function Cart() {
-  return <View />;
+  return (
+    <Container>
+      <Card>
+        <ProductLine>
+          <Image
+            source={{
+              uri:
+                'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg',
+            }}
+          />
+          <NamePrice>
+            <Name>Tênis de Caminhada Leve Confortável</Name>
+            <Price>R$ 179,90</Price>
+          </NamePrice>
+          <RemoveButton>X</RemoveButton>
+        </ProductLine>
+        <AmountLine>
+          <AmountControl>
+            <DecButton>-</DecButton>
+            <Amount>10</Amount>
+            <AddButton>+</AddButton>
+          </AmountControl>
+          <Subtotal>R$ 1.790,90</Subtotal>
+        </AmountLine>
+        <TotalLine>
+          <TotalTitle>Total</TotalTitle>
+          <Total>R$ 1.790,90</Total>
+        </TotalLine>
+      </Card>
+    </Container>
+  );
 }
---
+++ src/pages/Cart/styles.js
@@ -0,0 +1,72 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  padding: 5px;
+  background: #000;
+`;
+
+export const ProductLine = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+export const Image = styled.Image`
+  height: 60px;
+  width: 60px;
+`;
+export const NamePrice = styled.View`
+  flex-direction: column;
+  justify-content: space-between;
+`;
+export const Name = styled.Text`
+  width: 150px;
+`;
+export const Price = styled.Text`
+  font-weight: bold;
+`;
+export const RemoveButton = styled.Text``;
+export const AmountLine = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  background: #ddd;
+  padding: 10px 0;
+  margin: 5px 0;
+`;
+export const DecButton = styled.Text``;
+export const Amount = styled.Text`
+  border: 1px solid #eee;
+  background: #fff;
+`;
+export const AddButton = styled.Text``;
+export const Subtotal = styled.Text`
+  font-weight: bold;
+  font-size: 14px;
+`;
+export const TotalLine = styled.View`
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 20px 0;
+`;
+export const TotalTitle = styled.Text`
+  font-size: 18px
+  color: #666;
+`;
+export const Total = styled.Text`
+  font-weight: bold;
+  font-size: 25px;
+`;
+
+export const Card = styled.View`
+  background: #fff;
+  border-radius: 4px;
+  padding: 15px;
+  margin: 0 15px;
+`;
+
+export const AmountControl = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+`;
