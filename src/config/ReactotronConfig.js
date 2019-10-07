395e38bb561751f92267cfedf4bd19ebd5062e2e
Andre Praeiro <andrepraeiro@gmail.com>
2019-10-07 00:28:22 -0400

    configure reactotron redux saga

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -1,10 +1,12 @@
 import Reactotron from 'reactotron-react-native';
 import {reactotronRedux} from 'reactotron-redux';
+import ReactotronSaga from 'reactotron-redux-saga';
 
 if (__DEV__) {
   const tron = Reactotron.configure({host: 'localhost'})
     .useReactNative()
     .use(reactotronRedux())
+    .use(ReactotronSaga())
     .connect();
   console.tron = tron;
   tron.clear();
